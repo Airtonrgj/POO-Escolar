9d58d6ea420ee527b784f900085bd44fb577886e
surufel <henriquefrancasmm@gmail.com>
2025-10-14 22:46:51 -0300

    feat class CadastroDisciplinas + ArrayList (String) lista_disciplina, feat methods addDisciplina, ListarDisciplinas, removeDisciplina

--- src/CadastroDisciplinas.java
+++ src/CadastroDisciplinas.java
@@ -1,2 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CadastroDisciplinas {
+    public ArrayList<String> lista_disciplina = new ArrayList<>();
+
+    public void addDisciplina(String nome_disciplina) {
+        lista_disciplina.add(nome_disciplina);
+    }
+    public ArrayList<String> ListarDisciplinas(){
+        return lista_disciplina;
+    }
+    public void removeDisciplina(String nome_disciplina){
+        lista_disciplina.remove(nome_disciplina);
+    }
 }

6c61d302de38a548c499f96ff31368e28d2bbbc5
surufel <henriquefrancasmm@gmail.com>
2025-10-18 18:30:08 -0300

    Changes: menuDisciplina, a funcionalidade foi transferida para dentro do método da classe. Facilita o trabalho e melhora o espaço na main

--- src/CadastroDisciplinas.java
+++ src/CadastroDisciplinas.java
@@ -14,13 +14,60 @@ public class CadastroDisciplinas {
     public void removeDisciplina(String nome_disciplina){
         lista_disciplina.remove(nome_disciplina);
     }
-    public void menuDisciplina(){
-        System.out.println("----------------------------");
-        System.out.println("GESTÃO DE DISCIPLINAS - Escolha uma das opções:");
-        System.out.println("1 -- Adicionar disciplina");
-        System.out.println("2 -- Listar disciplinas");
-        System.out.println("3 -- Remover disciplina");
-        System.out.println("4 -- Sair");
-        System.out.println("----------------------------");
+
+    public void menuDisciplina() {
+        Scanner input_menudiscip = new Scanner(System.in);
+        int menu_disciplina_op;
+        do{
+            System.out.println("----------------------------");
+            System.out.println("GESTÃO DE DISCIPLINAS - Escolha uma das opções:");
+            System.out.println("1 -- Adicionar disciplina");
+            System.out.println("2 -- Listar disciplinas");
+            System.out.println("3 -- Remover disciplina");
+            System.out.println("4 -- Sair");
+            System.out.println("----------------------------");
+            menu_disciplina_op = input_menudiscip.nextInt();
+            input_menudiscip.nextLine();
+            switch (menu_disciplina_op) {
+                case 1:
+                    System.out.println("Insira o nome da nova matéria");
+                    String novadisciplina = input_menudiscip.nextLine();
+                    if(novadisciplina.isEmpty()){
+                        System.out.println("Nome da disciplina está vazio. Tente novamente");
+                        break;
+                    }
+                    if (lista_disciplina.contains(novadisciplina)){
+                        System.out.println("A matéria já existe dentro da lista.");
+                        System.out.println("A lista se mantêm inalterada.");
+                    } else {
+                        addDisciplina(novadisciplina);
+                        System.out.println("Nova Disciplina: " + novadisciplina);
+                        break;}
+                case 2:
+                    System.out.println("Disciplinas: " + ListarDisciplinas());
+                    break;
+                case 3:
+                    System.out.println("Digite o nome da disciplina");
+                    String disciplina_removida = input_menudiscip.nextLine();
+                    if (lista_disciplina.contains(disciplina_removida)){
+                        System.out.println("Removendo disciplina . . .");
+                        removeDisciplina(disciplina_removida);
+                        System.out.println("Disciplina ''"+disciplina_removida+"'' removida com sucesso.");
+                        break;
+                    }
+                    else if (disciplina_removida.isEmpty()){
+                        System.out.println("O nome está vazio. Tente novamente.");
+                        break;
+                    }else{
+                        System.out.println("O nome está errado. Tente novamente.");
+                    }
+                    break;
+                case 4:
+                    System.out.println("SAINDO . . .");
+                    break;
+                default:
+                    System.out.println("Opção Inválida, tente novamente!");
+            }
+        } while (menu_disciplina_op != 4);
     }
 }

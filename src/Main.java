cea9b0964f3b1f0afc5fec39caebd3b158f66b35
surufel <henriquefrancasmm@gmail.com>
2025-10-18 18:31:32 -0300

    Feat. method menuPrincipal, o menu principal permite se mover entre o outro menu de disciplinas, o de alunos (ainda a ser feito) e sair.

--- src/Main.java
+++ src/Main.java
@@ -3,55 +3,35 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static void menuPrincipal(){
+        System.out.println("***********************************************");
+        System.out.println("Menu da Escola FulanoDeTal - Escolha uma opção:");
+        System.out.println("1 - Gerenciamento de Disciplinas");
+        System.out.println("2 - Gerenciamento de Alunos (WIP)");
+        System.out.println("3 - Sair");
+        System.out.println("***********************************************");
+    }
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        CadastroDisciplinas disciplinas = new CadastroDisciplinas();
-        int menu_disciplina_op;
-        do{
-            disciplinas.menuDisciplina();
-            menu_disciplina_op = input.nextInt();
-            input.nextLine();
-            switch (menu_disciplina_op) { // Provável que tudo isso abaixo terá que virar um method so para
-                case 1: // Conseguir portar um menu principal, e esse menu. Ainda vou ver isso.
-                    System.out.println("Insira o nome da nova matéria");
-                    String novadisciplina = input.nextLine();
-                    if(novadisciplina.isEmpty()){
-                        System.out.println("Nome da disciplina está vazio. Tente novamente");
-                        break;
-                    }
-                    if (disciplinas.lista_disciplina.contains(novadisciplina)){
-                        System.out.println("A matéria já existe dentro da lista.");
-                        System.out.println("A lista se mantêm inalterada.");
-                    } else {
-                    disciplinas.addDisciplina(novadisciplina);
-                        System.out.println("Nova Disciplina: " + novadisciplina);
-                    break;}
+        int menu_op;
+        do {
+            menuPrincipal();
+            Scanner input_menu = new Scanner(System.in);
+            menu_op = input_menu.nextInt();
+            switch (menu_op) {
+                case 1:
+                    CadastroDisciplinas disciplinas = new CadastroDisciplinas();
+                    disciplinas.menuDisciplina();
+                    break;
                 case 2:
-                    System.out.println("Disciplinas: " + disciplinas.ListarDisciplinas());
-                    break;
+                    System.out.println("EM PROGRESSO (W.I.P.)");
                 case 3:
-                    System.out.println("Digite o nome da disciplina");
-                    String disciplina_removida = input.nextLine();
-                    if (disciplinas.lista_disciplina.contains(disciplina_removida)){
-                        System.out.println("Removendo disciplina . . .");
-                        disciplinas.removeDisciplina(disciplina_removida);
-                        System.out.println("Disciplina ''"+disciplina_removida+"'' removida com sucesso.");
-                        break;
-                    }
-                    else if (disciplina_removida.isEmpty()){
-                        System.out.println("O nome está vazio. Tente novamente.");
-                        break;
-                    }else{
-                        System.out.println("O nome está errado. Tente novamente.");
-                    }
-                    break;
-                case 4:
                     System.out.println("SAINDO . . .");
                     break;
                 default:
-                    System.out.println("Opção Inválida, tente novamente!");
+                    System.out.println("Opção Inválida, tente novamente.");
             }
-        } while (menu_disciplina_op != 4);
+        } while (menu_op != 3);
     }
 
 

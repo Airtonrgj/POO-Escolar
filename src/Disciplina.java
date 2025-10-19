5b1f3f5d372fe8b8c0c803a04bff2376f5100db0
surufel <henriquefrancasmm@gmail.com>
2025-10-19 20:26:29 -0300

    Foi adicionado um Construtor Disciplina, getters, e outros métodos necessários para a execução.

--- src/Disciplina.java
+++ src/Disciplina.java
@@ -1,4 +1,43 @@
-public class Disciplina {
-    private String codigo;
+import java.util.*;
+
+// Classe que representa uma Disciplina
+class Disciplina implements Comparable<Disciplina> {
     private String nome;
+    private String codigo;
+
+    public Disciplina(String nome, String codigo) {
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    @Override
+    public String toString() {
+        return codigo + " - " + nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Disciplina)) return false;
+        Disciplina that = (Disciplina) o;
+        return Objects.equals(codigo, that.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+
+    @Override
+    public int compareTo(Disciplina o) {
+        return this.codigo.compareTo(o.codigo);
+    }
 }
